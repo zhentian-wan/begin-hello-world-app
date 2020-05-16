d4b7b048e1c42bba95d9171f9d03975e7020ab79
zhentian-wan <answer881215@gmail.com>
2020-05-16 11:08:21 +0300

    fix

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -10,7 +10,7 @@ export async function handler(req: object) {
       "cache-control":
         "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
     },
-    bod: JSON.stringify(body),
+    body: JSON.stringify(body),
   };
 }
 
